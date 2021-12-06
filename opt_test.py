82259d49a41246df46a3e9c716ea7d1beba66103
rilianx <rilianx@gmail.com>
2021-12-06 17:35:34 -0300

    removing BR0 and improving strategy selection

--- opt_test.py
+++
@@ -1,8 +0,0 @@
-
-import sys
-import numpy as np
-
-x=float(sys.argv[2]); y=float(sys.argv[3])
-np.random.seed( 100000+int(sys.argv[1])*int((x+2)*(y+2)**2) ) 
-mu = (50 - x**2 - (2*y)**2)/50.0
-print (np.random.normal(mu, 0.05, 1)[0])
--- single_file/speculative_monitor.py
+++ single_file/speculative_monitor.py
@@ -306,8 +306,6 @@ class SpeculativeMonitor:
             
             if counters_file is not None:
                 save("counters.dat",self.counters)
-
-            if counter[len(counter)-1] >= 99: break
             i+=1
 
   @staticmethod
