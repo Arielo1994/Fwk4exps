12000d6fdbf135f66672219cc40351bffc0635ca
rilianx <rilianx@gmail.com>
2021-12-09 12:53:38 -0300

    extracting results

--- single_file/fact_exp_design.py
+++ single_file/fact_exp_design.py
@@ -12,7 +12,7 @@ def experimentalDesign():
     for x,y in itertools.product(*[[0.0, 1.0, 2.0],[0.0, 1.0, 2.0]]):
         params =  {"x": x, "y": y}
         if params == base_params: continue
-        S2 = Strategy.create_strategy('opt_test', 'python .\opt_test.py 0.5',  '{x} {y}', params)
+        S2 = Strategy.create_strategy('opt_test', 'python opt_test.py 0.5',  '{x} {y}', params)
         S = f4e.bestStrategy(S, S2)
 
     f4e.output += str(S.params)
