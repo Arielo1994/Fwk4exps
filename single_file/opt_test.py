a836bc4093c7f429db9fb0e61ec058d3d61389a3
rilianx <rilianx@gmail.com>
2021-12-09 13:06:06 -0300

    factorial design

--- single_file/opt_test.py
+++ single_file/opt_test.py
@@ -3,6 +3,6 @@ import numpy as np
 
 x=float(sys.argv[3]); y=float(sys.argv[4])
 s=float(sys.argv[1])
-np.random.seed( int(sys.argv[2])*1234*(x+5)*(y+5) ) 
+np.random.seed( int(int(sys.argv[2])*1234*(x+5)*(y+5)) ) 
 mu = -((x)**2 + (2*(y))**2)
 print (np.random.normal(mu, s, 1)[0])
