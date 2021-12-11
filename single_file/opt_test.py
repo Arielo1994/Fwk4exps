90ce136edb08c3b9182d610383ad92049bf99030
rilianx <rilianx@gmail.com>
2021-12-11 10:28:28 -0300

    seed in opt_test

--- single_file/opt_test.py
+++ single_file/opt_test.py
@@ -1,8 +1,12 @@
 import sys
 import numpy as np
+import hashlib  
 
 x=float(sys.argv[3]); y=float(sys.argv[4])
 s=float(sys.argv[1])
-np.random.seed( int(int(sys.argv[2])*1234*(x+5)*(y+5)) ) 
+
+str= sys.argv[1]+sys.argv[2]+sys.argv[3]+sys.argv[4]
+hash=int(hashlib.sha1(str.encode("utf-8")).hexdigest(), 16) % (2 ** 32) 
+np.random.seed(hash) 
 mu = -((x)**2 + (2*(y))**2)
 print (np.random.normal(mu, s, 1)[0])
